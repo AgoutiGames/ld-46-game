7a14549923b20523132d0831fb7c5e1b884c3ba4
VectorWolf <mr.joe63@googlemail.com>
2020-04-18 13:40:59 +0200

    Add dirt-grass tileset and first sample map

---
+++ data/tilesets/dirt_and_grass.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="dirt_and_grass" tilewidth="16" tileheight="16" tilecount="20" columns="4">
+ <image source="dirt_and_grass.png" width="64" height="80"/>
+ <terraintypes>
+  <terrain name="Grass" tile="13"/>
+  <terrain name="Dirt" tile="12"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0"/>
+ <tile id="1" terrain="1,1,0,1"/>
+ <tile id="2" terrain="0,0,0,1"/>
+ <tile id="3" terrain="0,0,1,0"/>
+ <tile id="4" terrain="1,0,1,1"/>
+ <tile id="5" terrain="0,1,1,1"/>
+ <tile id="6" terrain="0,1,0,0"/>
+ <tile id="7" terrain="1,0,0,0"/>
+ <tile id="8" terrain="0,0,1,1"/>
+ <tile id="9" terrain="0,1,0,1"/>
+ <tile id="12" terrain="1,1,1,1"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="16" terrain="1,1,0,0"/>
+ <tile id="17" terrain="1,0,1,0"/>
+</tileset>
